e92acfcafc7c1b539b89dfc9d7fc334958b5322b
SanjulaD <desanjula99@gmail.com>
2022-12-05 22:02:47 +0530

    feat: update user types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,15 +1,19 @@
 module Types
   class QueryType < Types::BaseObject
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    # /users
+    field :users, [Types::UserType], null: false
 
-    field :items, 
-    [Types::ItemType],
-    null: false, 
-    description: "Return a list of items"
+    def users
+      User.all
+    end
 
-    def items
-      Item.all
-    end 
+    # /user/:id
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
   end
 end
